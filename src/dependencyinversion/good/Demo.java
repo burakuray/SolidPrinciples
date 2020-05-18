52e0e3af40da9bdf2826d34b82fd0e774f19768a
Burak URAY <burak.uray@gmail.com>
2020-05-18 09:37:39 +0300

    Dependency Inversion/Injection demo added

--- src/dependencyinversion/good/Demo.java
+++ src/dependencyinversion/good/Demo.java
@@ -4,12 +4,17 @@ public class Demo {
     public static void main(String[] args) {
         FBMesenger fbMesenger = new FBMesenger();
         Twitter twitter = new Twitter();
+        Viber viber = new Viber();
 
         Galery galery = new Galery();
+        //Twitter
         galery.setShareableApp(twitter);
         galery.send();
-
+        //Mesenger
         galery.setShareableApp(fbMesenger);
+        galery.send();
+        //Viber
+        galery.setShareableApp(viber);
         galery.send();
     }
 }
